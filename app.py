9b9dd549f8d49b25f0c869cece0e2847d643ebfa
Vuritch <omarabderhman123@gmai.com>
2025-04-20 17:57:04 +0200

    last

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__,
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:omar@localhost/resort?charset=utf8mb4'
 app.config["SECRET_KEY"] = "123456789"
-
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # Initialize database
 db.init_app(app)
 
